f3f5526c76a97b635cf208999ab06bd1fe857ebc
cpetras-screamingbox <csaba.petras@screamingbox.com>
2019-04-16 16:41:15 +0200

    Disabling propType check to fix HMR issues in dev mode;

--- esm/components/CalendarWeek.js
+++ esm/components/CalendarWeek.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { forbidExtraProps, or, childrenOfType } from 'airbnb-prop-types';
 import CalendarDay from './CalendarDay';
-import CustomizableCalendarDay from './CustomizableCalendarDay';
-var propTypes = process.env.NODE_ENV !== "production" ? forbidExtraProps({
-  children: or([childrenOfType(CalendarDay), childrenOfType(CustomizableCalendarDay)]).isRequired
-}) : {};;
+import CustomizableCalendarDay from './CustomizableCalendarDay'; // Disabling propTypes check to fix HMR bug
+// https://github.com/gaearon/react-hot-loader/issues/304
+// https://github.com/airbnb/react-dates/issues/950
+// const propTypes = forbidExtraProps({
+//   children: or([childrenOfType(CalendarDay), childrenOfType(CustomizableCalendarDay)]).isRequired,
+// });
+
 export default function CalendarWeek(_ref) {
   var children = _ref.children;
   return React.createElement("tr", null, children);
-}
-CalendarWeek.propTypes = process.env.NODE_ENV !== "production" ? propTypes : {};
+} // CalendarWeek.propTypes = propTypes;
--- src/components/CalendarWeek.jsx
+++ src/components/CalendarWeek.jsx
@@ -4,9 +4,12 @@ import { forbidExtraProps, or, childrenOfType } from 'airbnb-prop-types';
 import CalendarDay from './CalendarDay';
 import CustomizableCalendarDay from './CustomizableCalendarDay';
 
-const propTypes = forbidExtraProps({
-  children: or([childrenOfType(CalendarDay), childrenOfType(CustomizableCalendarDay)]).isRequired,
-});
+// Disabling propTypes check to fix HMR bug
+// https://github.com/gaearon/react-hot-loader/issues/304
+// https://github.com/airbnb/react-dates/issues/950
+// const propTypes = forbidExtraProps({
+//   children: or([childrenOfType(CalendarDay), childrenOfType(CustomizableCalendarDay)]).isRequired,
+// });
 
 export default function CalendarWeek({ children }) {
   return (
@@ -16,4 +19,4 @@ export default function CalendarWeek({ children }) {
   );
 }
 
-CalendarWeek.propTypes = propTypes;
+// CalendarWeek.propTypes = propTypes;
